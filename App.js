ab0f0cde3f3b3c649fdf54c067e9f42112e0c771
ArslanBaris <arslan.bariss@outlook.com>
2023-10-07 21:49:06 +0300

    Navigation added

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home'
+import Login from './screens/Login'
+import SignUp from './screens/SignUp'
+
+const Stack = createNativeStackNavigator();
+
+function NormalStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  );
+}
+
+function App() {
+  return (
+   <NavigationContainer>
+    <NormalStack />
+   </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
